fe8a3dfc0836bb9f437ff3bc5c0cb52dce30fa20
Héctor Vera <juniomilnovecientos@gmail.com>
2022-08-14 22:43:56 -0400

    Add 2 sample ERC721 contracts and @openzeppelin/contracts package

---
+++ contracts/erc721-contracts/DumbPrestigeSkin.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract DumbPrestigeSkin is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("DumbPrestigeSkin", "DPS") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "http://localhost:3009/lolnft/";
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+}
